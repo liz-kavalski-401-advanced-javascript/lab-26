4b5c79d68f4a07039b6aef21bc74d17ee146d072
Liz <40321604+lizkavalski@users.noreply.github.com>
2019-09-04 20:45:25 -0700

    it looks okay

---
+++ src/components/counter.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../app.scss";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  increaseUp = e => {
+    e.preventDefault();
+    let counter = this.state.counter + 1;
+    this.setState({ counter });
+  };
+
+  decreaseDown = e => {
+    e.preventDefault();
+    let counter = this.state.counter - 1;
+    this.setState({ counter });
+  };
+  render() {
+    return (
+      <div>
+        <button id="add" onClick={this.increaseUp}>
+          Click Me to Add
+        </button>
+        <h4>{this.state.counter}</h4>
+        <button id="sub" onClick={this.decreaseDown}>
+          Click Me to Subtract
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../app.scss";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Hello!</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
